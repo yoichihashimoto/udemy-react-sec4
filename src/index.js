d46d1f7d1443e38ae10ef928e1995010199b766a
yoichihashimoto <110149436+yoichihashimoto@users.noreply.github.com>
2022-07-28 22:13:09 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// const
+
+// var vall = 'val変数';
+// console.log(vall)
+
+// vall = 'val変数2'
+
+// console.log(vall)
+
+// var vall = '変数の再宣言';
+
+// console.log(vall)
+
+let val2 = "let変数";
+console.log(val2);
+
+val2 = "let変数を上書き";
+console.log(val2);
+
+// let val2 = 'let変数を再宣言';
+
+const val3 = "const変数";
+console.log(val3);
+
+// val3 = 'const変数の上書き';
+
+// const val3 ='const変数を再宣言';
+
+const val4 = {
+  name: "yoichi",
+  age: 31
+};
+
+val4.name = "yoichi上書き";
+val4.addres = "kawagoe";
+console.log(val4.addres);
+
+const val5 = ["dog", "cat"];
+
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
